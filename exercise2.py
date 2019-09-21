0827e4d81c6a4f2b61a327e86f5fe0f220392ff2
LiuetenantMar <markyabis08@gmail.com>
2019-09-21 15:19:55 +0800

    Kaizend Session 2

---
+++ exercise2.py
@@ -0,0 +1,16 @@
+# If Statements and Comments Exercise
+
+# Make an if-else statement if year is between 2000 and 2100
+# (including both numbers), then print out:
+# "Welcome to the 21st century"
+# Else print out:
+# "You are before or after the 21st century"
+
+
+a = 1830
+b = 2000
+c = 2100
+if a > b and a < c:
+	print("Welcome to the 21st century")
+else:
+	print("You are before or after the 21st century")
---
+++ exercise3.py
@@ -0,0 +1,10 @@
+# Challenge - Functions Exercise
+
+# Create a function named tripleprint that takes a string as a parameter
+# and prints that string 3 times in a row.
+# So if I passed in the string "hello",
+# it would print "hellohellohello"
+
+def tripleprint():
+		print("Great" * 3)
+tripleprint()
